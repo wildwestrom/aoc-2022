0fd3217de84e2a28d6322afc4b2ab54338d0baa1
Christian Westrom <c.westrom@westrom.xyz>
2022-12-07 21:53:51 -0500

    refactor day 7
    refactor a little more

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -16,56 +16,43 @@ fn input_to_files(input: &str) -> Files {
     let mut pwd = PathBuf::new();
     let mut files = HashMap::new();
 
-    input
-        .lines()
-        .map(|l| {
-            if l.starts_with('$') {
-                let command_contents = l.chars().skip(2).collect::<String>();
-                if command_contents.starts_with("cd") {
-                    let destination = command_contents.chars().skip(3).collect::<String>();
-                    if destination == ".." {
-                        pwd.pop();
-                    } else {
-                        pwd.push(&destination);
-                    }
-                } else if command_contents.starts_with("ls") {
-                    return;
+    input.lines().for_each(|l| {
+        if l.starts_with('$') {
+            let command_contents = l.chars().skip(2).collect::<String>();
+            if command_contents.starts_with("cd") {
+                let destination = command_contents.chars().skip(3).collect::<String>();
+                if destination == ".." {
+                    pwd.pop();
                 } else {
-                    panic!("unexpected input")
-                }
-            } else {
-                if l.starts_with('d') {
-                    let _dirname = l.chars().skip(4).collect::<String>();
-                } else {
-                    let filename_chars = l.chars();
-                    let filename = filename_chars
-                        .clone()
-                        .skip_while(|c| c.is_numeric())
-                        .skip(1)
-                        .collect::<String>();
-                    let filesize = filename_chars
-                        .take_while(|c| c.is_numeric())
-                        .collect::<String>()
-                        .parse::<usize>()
-                        .expect("oh no, parsing string into usize failed");
-                    let mut newfilename = pwd.clone();
-                    newfilename.push(&filename);
-                    files.insert(newfilename, filesize);
+                    pwd.push(&destination);
                 }
             }
-        })
-        .collect_vec();
+        } else {
+            if !l.starts_with('d') {
+                let (size_str, filename_str) = l
+                    .split_whitespace()
+                    .collect_tuple::<(&str, &str)>()
+                    .unwrap();
+
+                let filename = PathBuf::from(filename_str);
+                let size = size_str.parse().unwrap();
+
+                let mut full_path = pwd.clone();
+                full_path.push(&filename);
+
+                files.insert(full_path, size);
+            }
+        }
+    });
     files
 }
 
-fn files_to_dirs_with_sizes(files: Files) -> Files {
+fn input_to_dirs(input: &str) -> Files {
+    let files = input_to_files(input);
     let mut dirs: Files = HashMap::new();
 
     files.iter().for_each(|(path, size)| {
-        // For each file, take their parents
         path.ancestors().for_each(|a| {
-            // Then find each directory that matches that parent path
-            // and add the file size of every file to the directory size
             if let Some(parent) = a.parent() {
                 dirs.entry(parent.to_path_buf())
                     .and_modify(|esize| *esize += size)
@@ -78,8 +65,7 @@ fn files_to_dirs_with_sizes(files: Files) -> Files {
 }
 
 pub fn part_one(input: &str) -> Option<usize> {
-    let files = input_to_files(input);
-    let dirs = files_to_dirs_with_sizes(files);
+    let dirs = input_to_dirs(input);
 
     let under_100k = dirs
         .iter()
@@ -91,23 +77,16 @@ pub fn part_one(input: &str) -> Option<usize> {
 }
 
 pub fn part_two(input: &str) -> Option<usize> {
-    let total_space = 70000000;
-    let unused_space_target = 30000000;
-    let used_space_target = total_space - unused_space_target;
-
-    let files = input_to_files(input);
-    let dir_hashmap = files_to_dirs_with_sizes(files);
-    let dirs = dir_hashmap.values();
+    let dirs_hashmap = input_to_dirs(input);
+    let dirs = dirs_hashmap.values();
 
     let total_disk_usage = dirs.clone().max().unwrap();
 
-    let file_we_want = dirs
-        .filter(|d| {
-            total_disk_usage - *d <= used_space_target
-        })
+    let smallest_that_frees_enough_space = dirs
+        .filter(|d| total_disk_usage - *d <= 40000000)
         .min()
         .unwrap();
-    Some(*file_we_want)
+    Some(*smallest_that_frees_enough_space)
 }
 
 fn main() {
